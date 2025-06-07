1bea9183228146b222841356892ea9ef005ab870
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-06-07 12:48:22 +0530

    Making changes to folder structure

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/GasAgency/', 
+  base: '/GasAgency/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      },
+    },
+  },
 });
