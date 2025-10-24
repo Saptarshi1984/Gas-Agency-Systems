ebc0d9974eef5307a5b65fbd46270599589bd127
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-10-24 09:34:59 +0530

    fix name in vite,config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/Gas-Agency=Systems/',
+  base: '/Gas-Agency-Systems/',
   build: {
     rollupOptions: {
       input: {
